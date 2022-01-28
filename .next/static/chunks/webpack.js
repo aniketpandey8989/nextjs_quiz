3c83ebc18574d1a2976ed79c3860f46d3fb31531
jss-codd <pandeyaniket546@gmail.com>
2022-01-28 16:43:51 +0530

    functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efbdae1194f8700a"; }
+/******/ 		__webpack_require__.h = function() { return "42b1441230ca7cb3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,71 +1,20 @@
 import {
   Button,
-  Center,
   Container,
-  Divider,
-  FormControl,
-  FormLabel,
   Heading,
-  HStack,
-  RadioGroup,
   SimpleGrid,
-  Text,
   Box,
   Image,
 } from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
-// import quizimg from "../../public/assets/images/Q4T.png";
-
-//   import { NextPageContext } from 'next';
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { questions } from "./quiz.json";
-//   import { getSingleQuiz } from '../../../utils/db';
-//   import { addAnswerApi } from '../../../utils/service';
 
-const test1 = [
-  {
-    question: "What is the scientific name of a butterfly?",
-    options: ["Apis", "Coleoptera", "Formicidae", "Rhopalocera"],
-    correctIndex: 3,
-  },
-  {
-    question: "How hot is the surface of the sun?",
-    options: ["1,233 K", "5,778 K", "12,130 K", "101,300 K"],
-    correctIndex: 1,
-  },
-];
-
-let singleQuiz = {
-  title: "How hot is the surface of the sun?",
-  options: [
-    {
-      optionId: 1,
-      title: "1,233 K",
-    },
-    {
-      optionId: 2,
-      title: "5,778 K",
-    },
-    {
-      optionId: 3,
-      title: "12,130 K",
-    },
-    {
-      optionId: 4,
-      title: "101,300 K",
-    },
-  ],
-};
-
-const ShowQuiz = (quiz, onSubmit) => {
+const ShowQuiz = (quiz, onSubmit, handleAnswer, handleNext,questionNumber) => {
   return (
     <div className="bgcolor">
       <div className="card_center">
         <Container
           maxW="5xl"
-          // mt={28}
-          // mb={5}
           borderWidth="1px"
           borderRadius="lg"
           p={6}
@@ -74,31 +23,38 @@ const ShowQuiz = (quiz, onSubmit) => {
         >
           <div>
             <Heading as="h2" className="headingtext">
-              5/ Which of these sh*tposts was written by Elon?
+             {quiz.question}
             </Heading>
             <SimpleGrid columns={2} spacingX="40px" spacingY="20px" mt={5}>
-              <div className="centerimg redbox">
-                <Box>
+            {quiz.answers.map((dt,index) => {
+             return <div key={index} onClick={() => handleAnswer(index)} className= {quiz.correctIndex === index ? "centerimg redbox" : "centerimg greenbox"}>
+                <Box >
                   <Image
-                    src="https://www.linkpicture.com/q/moon_1.jpg"
-                    alt="quizimg"
+                    src={dt.img}
+                    alt="logo"
                     className="quizstyle"
+                  />
+                  <Image
+                    src="https://www.linkpicture.com/q/checked-1.png"
+                    alt="quizimg"
+                    className="checkright"
+                  />
+                   <Image
+                    src="https://www.linkpicture.com/q/cancel-1.png"
+                    alt="quizimg"
+                    className="checkright"
                   />
                 </Box>
               </div>
-              <div className="centerimg greenbox">
-                <Box>
-                  <Image
-                    src="https://www.linkpicture.com/q/boa1.jpg"
-                    alt="quizimg"
-                    className="quizstyle"
-                  />
-                </Box>
-              </div>
+            })}
             </SimpleGrid>
             <SimpleGrid columns={1} spacingX="40px" spacingY="20px">
               <div className="btnstyle">
-                <Button colorScheme="blue">Next</Button>
+              {questionNumber === questions.length-1 ? 
+                <Button onClick={onSubmit} colorScheme="blue">Submit</Button>
+                :
+                <Button onClick={handleNext} colorScheme="blue">Next</Button>
+              }
               </div>
             </SimpleGrid>
           </div>
@@ -108,28 +64,34 @@ const ShowQuiz = (quiz, onSubmit) => {
   );
 };
 
-const SingleQuiz = (props) => {
-  const quiz = props.quiz;
+const SingleQuiz = () => {
+  const [questionNumber, setQuestionNumber] = useState(0)
 
-  const onSubmit = async (values, actions) => {
-    //   try {
-    //     const resp = await addAnswerApi( props.quizId, values);
-    //     const answerId = resp.data.data.answerId;
-    //     router.push(`/quiz/${props.quizId}/answer/${answerId}`);
-    //   } catch (error) {
-    //     console.log('error', error);
-    //   } finally {
-    //     actions.setSubmitting(false);
-    //   }
+  const quiz = questions[questionNumber];
+
+  const onSubmit = async (values) => {
+      try {
+        console.log(values)
+      } catch (error) {
+        console.log('error', error);
+      } finally {
+        console.log("finally")
+      }
   };
 
-  return <>{quiz && ShowQuiz(quiz, onSubmit)}</>;
+  const handleAnswer = (index) => {
+    console.log("handleAnswer clicked",index)
+  }
+
+  const handleNext = () => {
+    console.log("handle next clicked",questionNumber)
+    if(questionNumber < questions.length -1){
+      setQuestionNumber(questionNumber + 1)
+    }
+  }
+
+  return <>{quiz && ShowQuiz(quiz, onSubmit, handleAnswer,handleNext,questionNumber)}</>;
 };
 
-export async function getServerSideProps() {
-  const quizId = 1;
-  const quizData = questions;
-  return { props: { quiz: quizData, quizId } };
-}
 
 export default SingleQuiz;
