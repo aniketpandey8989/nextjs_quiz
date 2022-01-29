6fa12b5894663916ed56761238364f22c7f70a19
aniket <pandeyaniket546@gmail.com>
2022-01-29 12:35:06 +0530

    responsive_design

--- pages/index.js
+++ pages/index.js
@@ -1,142 +1,202 @@
 import {
-  Box, Button,
+  Box,
+  Button,
   Container,
-  Heading, Image, SimpleGrid
+  Heading,
+  Image,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { questions } from "./quiz.json";
 
 const SingleQuiz = () => {
-  const [questionNumber, setQuestionNumber] = useState(0)
-  const [score, setScore] = useState(0)
-  const [selectedAnswer, setSelectedAnswer] = useState(null)
-  const [submittingAnswer, setSubmittingAnswer] = useState(false)
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [submittingAnswer, setSubmittingAnswer] = useState(false);
   const quiz = questions[questionNumber];
 
   const handleAnswer = (index) => {
     if (submittingAnswer) {
       return;
     }
-    setSubmittingAnswer(true)
-    setSelectedAnswer(index)
+    setSubmittingAnswer(true);
+    setSelectedAnswer(index);
     if (quiz.correctIndex === index) {
       setScore(score + 1);
     }
 
     if (questionNumber <= questions.length - 1) {
       setTimeout(() => {
-        setQuestionNumber(questionNumber + 1)
-        setSelectedAnswer(null)
-        setSubmittingAnswer(false)
-      }, 2000)
+        setQuestionNumber(questionNumber + 1);
+        setSelectedAnswer(null);
+        setSubmittingAnswer(false);
+      }, 2000);
     }
-  }
+  };
 
   const resetState = () => {
-    setQuestionNumber(0)
-    setScore(0)
-    setSelectedAnswer(null)
-    setSubmittingAnswer(false)
-  }
+    setQuestionNumber(0);
+    setScore(0);
+    setSelectedAnswer(null);
+    setSubmittingAnswer(false);
+  };
 
-  return <>{(
-    <div className="bgcolor">
-      <div className="card_center">
-        <Container
-          maxW="5xl"
-          borderWidth="1px"
-          borderRadius="lg"
-          p={6}
-          boxShadow="xl"
-          className="cardstyle"
-        >
-          {(questionNumber <= questions.length - 1)
-            ? (
-              <div>
-                <Heading as="h2" my={"3rem"} maxWidth={"50rem"} mx={"auto"} className="headingtext">
-                  {`${questionNumber + 1}/15 ${quiz.question}`}
-                </Heading>
-                <SimpleGrid columns={2} spacingX="40px" spacingY="20px" mt={5}>
-                  {quiz.answers.map((dt, index) => {
-                    return <div key={index} onClick={() => handleAnswer(index)} className={quiz.correctIndex === index ? "centerimg redbox" : "centerimg greenbox"}>
-                      <Box >
-                        <Image
-                          src={dt.img}
-                          alt="logo"
-                          className="quizstyle"
-                        />
-                        {selectedAnswer === index && selectedAnswer !== null && (
-                          <>
-                            {selectedAnswer === quiz.correctIndex ? <Image
-                              src="https://www.linkpicture.com/q/checked-1.png"
-                              alt="quizimg"
-                              className="checkright"
-                            /> : <Image
-                              src="https://www.linkpicture.com/q/cancel-1.png"
-                              alt="quizimg"
-                              className="checkright"
-                            />}
-                          </>
-                        )}
-                      </Box>
-                    </div>
-                  })}
-                </SimpleGrid>
-                <SimpleGrid columns={1} spacingX="40px" spacingY="20px">
-                  <Heading as="h3" style={{ textAlign: "right" }} mt={10} className="headingtext">{`Score: ${score}/15`}</Heading>
-                </SimpleGrid>
-              </div>
-            ) : (
-              <div>
-                <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
-                  <Box>
-                    {" "}
-                    <div className="verticalcenter">
-                      <Heading as="h2" className="textleft">
-                        Your score: {score}/{questions.length} IMPRESSIVE!
-                      </Heading>
-                    </div>
-                  </Box>
-                  <Box>
-                    <div className="">
-                      <Heading as="h2" className="textsize">
-                        Share your 💩 performance on Twitter!{" "}
-                      </Heading>
-                    </div>
-                    <Box maxWidth={"25rem"}>
-                      <Box className="scoreCard">
-                        {" "}
-                        <Box display={"flex"} alignItems={"center"}>
-                          <div className="scoreBoardText">
-                            <div>Score: {score}/{questions.length}</div>
-                            <div>
-                              Take the Elon Sh*tposts 💩 quiz and beat me!
-                            </div>
-                            <Box display={"flex"} justifyContent={"center"}>
-                              <Button onClick={resetState} className="btncolor quizMeBtn">Quiz Me</Button>
+  return (
+    <>
+      {
+        <div className="bgcolor">
+          <div className="card_center">
+            <Container
+              maxW="5xl"
+              borderWidth="1px"
+              borderRadius="lg"
+              p={6}
+              boxShadow="xl"
+              className="cardstyle"
+            >
+              {questionNumber <= questions.length - 1 ? (
+                <div>
+                  <Heading className="headingtext">
+                    {`${questionNumber + 1}/15 ${quiz.question}`}
+                  </Heading>
+                  <SimpleGrid
+                    columns={2}
+                    spacingX="40px"
+                    spacingY="20px"
+                    mt={5}
+                  >
+                    {quiz.answers.map((dt, index) => {
+                      return (
+                        <div
+                          key={index}
+                          onClick={() => handleAnswer(index)}
+                          className={
+                            quiz.correctIndex === index
+                              ? "centerimg redbox"
+                              : "centerimg greenbox"
+                          }
+                        >
+                          <Box>
+                            <Image
+                              src={dt.img}
+                              alt="logo"
+                              className="quizstyle"
+                            />
+                            {selectedAnswer === index &&
+                              selectedAnswer !== null && (
+                                <>
+                                  {selectedAnswer === quiz.correctIndex ? (
+                                    <Image
+                                      src="https://www.linkpicture.com/q/checked-1.png"
+                                      alt="quizimg"
+                                      className="checkright"
+                                    />
+                                  ) : (
+                                    <Image
+                                      src="https://www.linkpicture.com/q/cancel-1.png"
+                                      alt="quizimg"
+                                      className="checkright"
+                                    />
+                                  )}
+                                </>
+                              )}
+                          </Box>
+                        </div>
+                      );
+                    })}
+                  </SimpleGrid>
+                  <SimpleGrid columns={1} spacingX="40px" spacingY="20px">
+                    <Heading
+                      as="h3"
+                      mt={10}
+                      className="outofscore"
+                    >{`Score: ${score}/15`}</Heading>
+                  </SimpleGrid>
+                </div>
+              ) : (
+                <div>
+                  <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
+                    <Box>
+                      {" "}
+                      <div className="verticalcenter">
+                        <Heading as="h2" className="textleft">
+                          Your score: {score}/{questions.length} IMPRESSIVE!
+                        </Heading>
+                      </div>
+                    </Box>
+                    <Box>
+                      <div className="">
+                        <Heading as="h2" className="textsize">
+                          Share your 💩 performance on Twitter!{" "}
+                        </Heading>
+                      </div>
+                      <Box maxWidth={"25rem"} margin={"0 auto"}>
+                        <Box className="scoreCard">
+                          {" "}
+                          <Image
+                            src="https://i.postimg.cc/jdFLNFt5/image-3-2-3.png"
+                            alt="quizimg"
+                            className="crrdtopimg"
+                          />
+                          <div className="scoretext">
+                            I only recognized {score} out of my own{" "}
+                            {questions.length} sh*tposts. Damn.
+                          </div>
+                          <div className="boxborder">
+                            <Box display={"flex"} alignItems={"center"}>
+                              <div className="scoreBoardText">
+                                <div className="scorestyle">
+                                  Score: {score}/{questions.length}
+                                </div>
+                                <div>
+                                  Take the Elon Sh*tposts 💩 quiz and beat me!
+                                </div>
+                                <Box display={"flex"} justifyContent={"center"}>
+                                  <Button
+                                    onClick={resetState}
+                                    className="btncolor quizMeBtn"
+                                  >
+                                    Quiz Me
+                                  </Button>
+                                </Box>
+                              </div>
+
+                              <Image
+                                src="https://i.postimg.cc/zXpHN5vV/Group-519.png"
+                                alt="quizimg"
+                                className="quizstyle"
+                                maxWidth={"8rem"}
+                              />
                             </Box>
                           </div>
                           <Image
-                            src="https://i.postimg.cc/zXpHN5vV/Group-519.png"
+                            src="https://i.postimg.cc/Y0VM4TYB/image-3-2-1-1.png"
                             alt="quizimg"
                             className="quizstyle"
-                            maxWidth={"8rem"}
+                            // maxWidth={"8rem"}
                           />
                         </Box>
                       </Box>
+                      <Box
+                        display={"flex"}
+                        justifyContent={"center"}
+                        className="btnstyle"
+                      >
+                        <Button colorScheme="blue" className="btncolor">
+                          Tweet My Score
+                        </Button>
+                      </Box>
                     </Box>
-                    <Box display={"flex"} justifyContent={"center"} className="btnstyle">
-                      <Button colorScheme="blue" className="btncolor">Tweet My Score</Button>
-                    </Box>
-                  </Box>
-                </SimpleGrid>
-              </div>
-            )}
-        </Container>
-      </div>
-    </div>
-  )}</>
+                  </SimpleGrid>
+                </div>
+              )}
+            </Container>
+          </div>
+        </div>
+      }
+    </>
+  );
 };
-
 
 export default SingleQuiz;
